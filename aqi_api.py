6cdfac0595f08781f4686d117fc2c9fa10aa299f
_shakya._.sarthak_ <122764949+akira-lawliet@users.noreply.github.com>
2025-03-07 08:13:05 +0545

    changes

--- aqi_api.py
+++
@@ -1,7 +0,0 @@
-
-
-# Your API Key
-waqi_api_key = '397ac0d0d4eee768a08deec6a1e1a412d2bf95c8'
-openaqi_api_key = '76b5c11b822c0f6ad53709de0bde206c678f1cac174bc27393349fe9d9f33efd'
-iqair_api_key = 'f4b3b3b4-0b3b-4b3b-8b3b-0b3b4b3b4b3b'
-open_weather_api_key = '9cbc789da13f0719197d102c2abfd23c'
